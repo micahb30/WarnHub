2267804cc6e727e42dd6e46027c44cb28a718064
Andrew Nguyen <andrew.nguyen.waterloo@gmail.com>
2017-03-26 11:02:53 -0400

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,4 +1,3 @@
-//testing
 function getDisease() {
 	var symptoms = new Array();
 	var multiSelect = document.getElementById("symptoms");
@@ -18,9 +17,41 @@ function getDisease() {
 			sendArray[i] = 0;
 		}
 	}
+
 	//sendArray is now 1s and 0s based on whether symptom exists or not
+	// Sending and receiving data in JSON format using POST mothod
+	//
+	xhr = new XMLHttpRequest();
+	var url = "https://ussouthcentral.services.azureml.net/workspaces/ad5113b472ae4bea907d66f675f48c21/services/f031c70202334304b90c8e41bdef0c50/execute?api-version=2.0&details=true";
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.setRequestHeader("Authorization", "vGOQhvqJdfIrQoR92FiGfj5FRNoP/VQOgTlFovCN0QcU11DD1F++GWKe1ufygd7dCzwDvKOB5j5WbeEGYP1dZA==")
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.setRequestHeader("Access-Control-Allow-Origin", "true");
+	xhr.onreadystatechange = function () { 
+	    if (xhr.readyState == 4 && xhr.status == 200) {
+	        var json = JSON.parse(xhr.responseText);
+	        console.log(json.ColumnNames[1] + ", " + json.Values[1])
+	    }
+	}
+	var data = JSON.stringify(sendArray);
+	xhr.send(data);
+
+	// Sending a receiving data in JSON format using GET method
+	//
+	xhr = new XMLHttpRequest();
+	var url = "url?data=" + encodeURIComponent(JSON.stringify(sendArray));
+	xhr.open("GET", url, true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function () { 
+	    if (xhr.readyState == 4 && xhr.status == 200) {
+	        var json = JSON.parse(xhr.responseText);
+	    }
+	}
+	xhr.send();
+
+	disease = ""; //Put json response here
 	var name = getFirstWord(document.getElementById("name-input").value);
-	var disease = "TRISOMY 21";
 	writeDisease(disease, name);
 }
 
